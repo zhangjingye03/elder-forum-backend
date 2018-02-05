f055cc5a466890521887bf13592ab6d7f187d0f6
zhangjingye03 <934526987@qq.com>
2018-02-05 13:21:08 +0800

    Fix the bug causing the removal of floor 1 * 修正删除一楼的bug

--- api/forum/deleteTopicReply.php
+++ api/forum/deleteTopicReply.php
@@ -11,6 +11,9 @@
 	if (!is_admin() || !is_category_owner($cid) || !is_topic_reply_owner($cid, $tid, $rid))
 		die_with_code(403);
 
+	if ($rid < 2)
+		die_in_json("failed", "不能删除帖子的一楼哦，请直接删除帖子。");
+
 	try {
 		$q = new SQLStatement;
 		$q->deleteFrom("category_{$cid}_{$tid}")
