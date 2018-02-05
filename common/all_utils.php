1a56cec2247a949b2b5d9d081681b0f3336355a1
zhangjingye03 <934526987@qq.com>
2018-02-05 12:12:04 +0800

    Rename `check_args.php' to `arg_utils.php'

--- common/all_utils.php
+++ common/all_utils.php
@@ -6,12 +6,11 @@
  * =========================================
  */
 
+	require_once("arg_utils.php");
 	require_once("array_utils.php");
-	require_once("check_args.php");
 	require_once("check_captcha.php");
 	require_once("cool_die.php");
 	require_once("db_utils.php");
 	require_once("forum_utils.php");
-	require_once("url_utils.php");
 	require_once("user_utils.php");
 ?>
